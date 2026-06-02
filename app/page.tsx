73a910ad43528ebe1ecbc75bc64982ba1c9976db
Neelam-Fiaz <neelamfiaz1@gmail.com>
2026-06-02 19:25:12 +0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🎵 TuneDNA AI</h1>
+      <p>Discover your music personality from Spotify</p>
+    </main>
+  );
+}
