854d7e760bed7dc58c68356bffbea61df89861a7
Neelam-Fiaz <neelamfiaz1@gmail.com>
2026-06-02 19:26:01 +0500

    Create Hero.tsx

---
+++ app/components/Hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <section style={{ padding: "80px", textAlign: "center" }}>
+      <h1>Discover Your Music Personality 🎧</h1>
+      <p>Connect Spotify and unlock your Music DNA</p>
+    </section>
+  );
+}
